6274ce7b1fb94070f0d6d918805c425a72155cca
Quadrate Tech Solutions <info@quadrate.lk>
2021-01-16 19:28:44 +0530

    Commit program

--- Program.cs
+++ Program.cs
@@ -4,35 +4,17 @@ namespace EFCoreDemo
 {
     class Program
     {
-        public static EmployeeContext empContext = new EmployeeContext();
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            //Console.WriteLine("Welcome");
+            var dbCon = new EmployeeContext();
 
-            //var repo = new EmployeeRepo(empContext);
-            //repo.Create("Fazrin", "Farook", "Address", "123456789", "98765431");
+            // provider = new EmployeeProvider(new EmployeeContext());
+
+            //var employee = provider.Get(1);
+            Console.WriteLine("Welcome");
+
+            var repo = new EmployeeRepo(dbCon);
+            repo.Create("Fazrin", "Farook", "Address", "123456789", "98765431");
         }
-
-        public static Employee Create(string firstName, string lastName, string address, string homePhone, string cellPhone)
-        {
-            var data = empContext.Add(new Employee { FirstName = firstName, LastName = lastName, Address = address, HomePhone = homePhone, Mobile = cellPhone });
-            empContext.SaveChanges();
-            return data.Entity;
-        }
-
-        public static void Delete(Employee emp)
-        {
-            empContext.Remove(emp);
-            empContext.SaveChanges();
-        }
-
-
-        public static Employee Update(Employee employee)
-        {
-            var data = empContext.Update(employee);
-            empContext.SaveChanges();
-            return data.Entity;
-        }
-
     }
 }
